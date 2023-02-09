b81ad997ec48d772b131a6b33ea567d946ae7744
dmalitesta <d.malitesta@gmail.com>
2023-02-09 16:04:29 +0100

    Config files added with results.

--- run_results.sh
+++ run_results.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 declare -a StringArray=(
-'config_files/visual_clothing'
-'config_files/visual_sports'
-'config_files/visual_toys'
-'config_files/visual_office'
-'config_files/visual_beauty'
-'config_files/textual_clothing'
-'config_files/textual_sports'
-'config_files/textual_toys'
-'config_files/textual_office'
-'config_files/textual_beauty'
-'config_files/visual_textual_clothing'
-'config_files/visual_textual_sports'
-'config_files/visual_textual_toys'
-'config_files/visual_textual_office'
-'config_files/visual_textual_beauty'
+'visual_clothing'
+'visual_sports'
+'visual_toys'
+'visual_office'
+'visual_beauty'
+'textual_clothing'
+'textual_sports'
+'textual_toys'
+'textual_office'
+'textual_beauty'
+'visual_textual_clothing'
+'visual_textual_sports'
+'visual_textual_toys'
+'visual_textual_office'
+'visual_textual_beauty'
 )
 
 for conf in "${StringArray[@]}"; do
